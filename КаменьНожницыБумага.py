73ffff6c7704a24d0c55915a1c24995867897cd0
seriyrus <55887557+seriyrus@users.noreply.github.com>
2024-01-16 19:36:18 +0300

    Main

---
+++ КаменьНожницыБумага.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+tk = Tk()
+tk.title("SSSR")
+tk.geometry('400x400')
+Banner = Label(tk, text = "КамЕнь~Ножницы~Бумага", font=("Times new Roman", 25))
+Banner.grid(row=1)
+sc = [0,0]
+score = Label(tk, text = f"score: {sc[0]}:{sc[1]}", font=("Times new Roman", 16))
+score.grid(row=2)
+
+"""
+ebveibvfeg
+score: 1:1
+    
+    
+wf  wef wfe    
+    
+"""
+tk.mainloop()
